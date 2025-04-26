c9789688d033ee821b37bb412ffad92480ecad15
Yafi Thai <yathai@ucsc.edu>
2025-04-25 18:13:27 -0700

    oled

--- Software/Common/oledControl.py
+++ Software/Common/oledControl.py
@@ -2,27 +2,24 @@
 # SPDX-FileCopyrightText: 2017 James DeVito for Adafruit Industries
 # SPDX-License-Identifier: MIT
 
-import time
-import busio
+# SPDX-License-Identifier: MIT
+import time, board, busio, warnings
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 from adafruit_lc709203f import LC709203F
-import pigpio
+from adafruit_extended_bus import ExtendedI2C as I2C
 
+# (A) Silence the irrelevant warning
+warnings.filterwarnings("ignore", category=RuntimeWarning,
+                        message="I2C frequency is not settable*")
 
-# Set up GPIO reader
-pi = pigpio.pi()
+# (B) OLED on the hardware bus /dev/i2c-1 (GPIO 2/3)
+i2c_oled = busio.I2C(board.SCL, board.SDA)
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c_oled)
 
-pi.set_mode(22, pigpio.ALT5) 
-pi.set_mode(23, pigpio.ALT5) 
-
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-
-sensor = LC709203F(i2c)  # Using pin 15 and 16 (GPIO22/23) must use ALT5
-
-# Create the SSD1306 OLED display (128×32)
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c) # Using pin 3 and 5 (GPIO2/3)
+# (C) Battery gauge on the bit-banged bus /dev/i2c-3 (GPIO 22/23)
+i2c_batt = I2C(3)     # freq=None suppresses the warning too
+sensor   = LC709203F(i2c_batt)
 
 # Clear display.
 disp.fill(0)
@@ -48,7 +45,7 @@ while True:
 
     # format text
     voltage_text   = f"Voltage: {voltage:0.3f} V"
-    percent_text   = f"Charge: {percent:3.0f}%"
+    percent_text   = f"Charge:  {percent:3.0f}%"
 
     # draw text
     draw.text((0, top + 0),  voltage_text, font=font, fill=255)
